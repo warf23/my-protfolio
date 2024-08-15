859f7a0c3cf81d7ec8b7a764c12f162c1db39470
Warf23 <simo.agrat1@gmail.com>
2024-08-15 21:46:52 +0100

    add 3d

--- src/components/LaptopIcon/LaptopIcon.tsx
+++ src/components/LaptopIcon/LaptopIcon.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './LaptopIcon.css';
-// import ThreeScene from './ThreeScene';
+import ThreeScene from './ThreeScene';
 import LaptopICONn from '../../assets/laptop_image/Responsive Device.png';
 const LaptopIcon: React.FC = () => {
   return (
     <div className="laptop-icon">
-      <img src={LaptopICONn} alt="laptop icon" className="laptop-icon" />
+      <ThreeScene/>
     </div>
   );
 };
